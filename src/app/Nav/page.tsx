5461f67cba0aabe573a665fc309a213019201f5c
iyin141 <66314242+iyin141@users.noreply.github.com>
2025-06-06 22:13:19 +0100

    fix_5

--- src/app/Nav/page.tsx
+++ src/app/Nav/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from "next/image"
-import logo from '../img/logo.png'
+import logo from '../img/logo_2.png'
 import Link from "next/link"
 import { Inter } from 'next/font/google'
 
@@ -8,16 +8,22 @@ import { Inter } from 'next/font/google'
   subsets: ['latin'],
   weight: ['400', '700'], // optional
   variable: '--font-inter', // optional: for CSS variable usage
-})
+  })
+const link_style = 'flex flex-col w-[90px] rounded-[5px] h-[35px]  text-center justify-center'
 
 
 const Nav = () => {
   return (
-     <div className="flex justify-between  gap-5 pt-10 md:pl-12 md:pr-12 max-sm:pl-8 max-sm:pr-8 max-sm:pb-8" >
-      <div>
-          <Image src={logo} className="w-[80px] " alt=""/>
+     <div className="flex justify-between  md:pt-4 md:pb-3 pt-5  bg-[#F5F7FA] md:pl-12 md:pr-12 max-sm:pl-8 max-sm:pr-8 max-sm:pb-5  " >
+      <div className={`  ${inter.className} flex  `}>
+        <Image src={logo} className="w-[30px] h-[30px] " alt="" />
+        <h1 className='font-bold text-[1.2rem] text-[#263238]'>To-Do </h1>
       </div>
-     <span className={` ${inter.className}   flex flex-col text-[0.9rem] justify-center items-center pl-10 pr-10 max-sm:pl-6 max-sm:pr-6 bg-white max-sm:bg-[#326EF7] max-sm:text-white h-[40px] max-sm:h-[30px] text-center rounded-[5px] font-bold tracking-wide`}> <Link href='/Verify/Login' >Login</Link></span>
+      <div className='font-extralight h-[30px] flex flex-col justify-center max-sm:hidden  '><p>Embark on a productivity journey  with to-do app!</p></div>
+      <div className='flex justify-between gap-5 '>
+        <span className={` ${inter.className} ${link_style} text-[#4CAF4F] hover:text-white hover:bg-[#4CAF4F]  `}> <Link href='/Verify/Login' >Login</Link></span>
+       <span className={` ${inter.className} ${link_style} hover:text-[#4CAF4F] text-white bg-[#4CAF4F] hover:bg-[#F5F7FA]  ` }> <Link href='/Verify/Signup' >Sign up</Link></span>
+      </div>
     </div> 
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,33 @@ import { Inter } from 'next/font/google'
 import Nav from "./Nav/page";
 import Link from 'next/link'
 import Image from 'next/image';
-import hero_3 from "../../src/app/img/hero_3.png"
 import { useEffect } from 'react';
 import { useAuthStore } from './Components/Values';
 import { verify } from './Components/Send';
+import guy from "../app/img/guy.png"
 
   const inter = Inter({
   subsets: ['latin'],
   weight: ['400', '700'], // optional
   variable: '--font-inter', // optional: for CSS variable usage
 })
+
+const link_style = 'flex flex-col w-[130px] rounded-[5px] h-[55px]  text-center justify-center'
+const animation = ` @keyframes bounce-slow {
+            0%, 100% {
+              transform: translateY(-15%);
+              animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+            }
+            50% {
+              transform: translateY(0);
+              animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+            }
+          }
+
+          .bounce-slow {
+            animation: bounce-slow 2s infinite;
+          }`
+
 
 
 const Home = () => {
@@ -30,14 +47,27 @@ const Home = () => {
 }, [token]);
 
   return (
-    <div className={` ${inter.className}  xl:bg-[url('./img/hero.png')] md:bg-[url('./img/hero_2.png')] xl:bg-contain bg-cover  bg-no-repeat xl:h-[100vh]  bg-top xl:bg-right max-sm:h-[80%]    `}>
-      <Nav />
-      <div className="md:w-[60%]  lg:pt-12 pt-8 md:pl-12 md:pr-12 pb-12 max-sm:pl-8 max-sm:pr-8 max-sm:pt-12   flex flex-col  md:gap-8 gap-12 max-sm:items-center max-sm:text-center max-sm:justify-center">
-        <h1 className="font-extrabold xl:text-[5.3vw] lg:text-[3.2rem] md:text-[2.3rem] text-[2.3rem] lg:leading-[4.6rem] lg:tracking-wide"> Track & manage <span className="underline text-[#326EF7]">your tasks</span> easily</h1>
-        <p className='text-[#5E6282] lg:leading-8 md:leading-5   max-md:text-[0.7rem]  max-sm:text-[1rem] text-[1.3vw]  max-sm:leading-[30px] font-light'>Embark on a productivity journey like never before with our revolutionary to-do app! Experience a seamless way to organize your life, where managing tasks becomes effortless and enjoyable..✨</p>
-        <Link href='/Verify/Signup' className='mt-1  pt-1 pb-1 flex flex-col lg:text-[1.2rem] justify-center items-center pl-6 pr-6 bg-[#326EF7]  h-[50px] text-center rounded-[6px]  tracking-wide w-[180px] text-white' >Get Started</Link>
+    <div className={` ${inter.className} `}>
+      <style>
+        {`${animation} `}
+      </style>
+
+
+      <div className=' bg-[#ABBED1] pb-12 flex flex-col  w-[100%] gap-12 '>
+        <Nav />
+        <div  className='md:pt-12 flex  max-sm:justify-center max-sm:flex-col-reverse gap-12 md:pl-12 md:pr-12 max-sm:pl-8 max-sm:pr-8 max-sm:pb-8  '>
+          <div className='md:w-[55%] flex flex-col gap-8'>
+            <h1 className='text-[5vw] max-sm:text-[10vw] font-semibold xl:leading-[76px] md:leading-[50px]'> <span className='text-[#4D4D4D]'>Track & manage your</span>  <span className='text-[#4CAF4F]'>tasks easily</span></h1>
+            <p className='text-[#717171] font-light'>Experience a seamless way to organinzing your life becomes effortless and enjoyable..✨</p>
+            <Link href='/Verify/Signup' className={` ${link_style} text-white bg-[#4CAF4F]  `}>Get started</Link>
+          </div>
+          <div className='bounce-slow w-[45%] max-sm:hidden'>
+            <Image src={guy} className='w-[80%]' alt='' />
+          </div>
+        </div>
       </div>
-      <footer><Image src={hero_3} className='md:hidden h-[40vh]' alt='' /></footer>
+     
+      
    </div>
   )
 }
